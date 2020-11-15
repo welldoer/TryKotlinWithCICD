9c92e0428bec616915f34549c1d817bff9e6c637
welldoer <welldoer@263.net>
2020-11-15 13:48:38 +0800

    修改 build.gradle.kts 支持单元测试。

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,13 @@ repositories {
 }
 dependencies {
     testImplementation(kotlin("test-junit5"))
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.0")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
---
+++ src/test/kotlin/SampleTest.kt
@@ -0,0 +1,6 @@
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+    @Test
+    fun sample() {}
+}
